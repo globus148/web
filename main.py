3b64a1cf4aa794b154eb933582d782882d4db219
utu <gulovskiu@gmail.com>
2025-04-28 23:42:05 +0300

    Автоматический ответ

--- main.py
+++ main.py
@@ -2,17 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     return "Привет, Яндекс!"
 
-@app.route('/list_prof/<list>')
-def show_professions(list):
-    professions = ['инженер-исследователь', 'пилот', 'строитель',
-                  'врач', 'климатолог', 'астрогеолог',
-                  'метеоролог', 'специалист по радиационной защите']
-    return render_template("index.html", list_type=list, professions=professions)
+
+@app.route("/answer")
+@app.route('/auto_answer')
+def answer():
+    book = {
+        'title': 'Анкета претендента',
+        'surname': 'Иванов',
+        'name': 'Иван',
+        'education': 'Высшее',
+        'profession': 'Инженер-исследователь',
+        'sex': 'male',
+        'motivation': 'Хочу стать частью великой миссии!',
+        'ready': 'Да'
+    }
+
+    return render_template("auto_answer.html", **book)
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
