5f9b109035a6ac68955da83d7624b060e65b1484
utu <gulovskiu@gmail.com>
2025-05-03 13:54:59 +0300

    Цвет каюты

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
 from flask import Flask, render_template, redirect
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class LoginForm(FlaskForm):
-    username = StringField('id астронавта', validators=[DataRequired()])
-    password = PasswordField('Пароль автронавта', validators=[DataRequired()])
-    username1 = StringField('id капитана', validators=[DataRequired()])
-    password1 = PasswordField('Пароль капитана', validators=[DataRequired()])
-    submit = SubmitField('Доступ')
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -22,12 +10,21 @@ def index():
     return "Привет, Яндекс!"
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        return redirect('/success')
-    return render_template('login.html', title='Аварийный доступ', form=form)
+@app.route('/table/<pol>/<int:age>')
+def table(pol, age):
+    if pol == "male":
+        wall_color = "blue"
+    elif pol == "female":
+        wall_color = "orange"
+    else:
+        return
+    saturate = "100"
+    if age < 21:
+        img = "child.png"
+        saturate = "50"
+    else:
+        img = "adult.png"
+    return render_template("login.html", wall_color=wall_color, image=img, saturate=saturate)
 
 
 if __name__ == '__main__':
