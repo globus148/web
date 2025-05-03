0a84d7b17cc218112615e5ff8217d2f8e3901751
utu <gulovskiu@gmail.com>
2025-05-04 01:40:52 +0300

    Личная карточка

--- main.py
+++ main.py
@@ -1,44 +1,24 @@
 from flask import Flask, url_for, render_template, request, redirect
-import os
-from werkzeug.utils import secure_filename
+import json
+import random
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'static/img'
-app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
-
-def get_images():
-    images = []
-    if os.path.exists(app.config['UPLOAD_FOLDER']):
-        for file in os.listdir(app.config['UPLOAD_FOLDER']):
-            if file.split('.')[-1].lower() in app.config['ALLOWED_EXTENSIONS']:
-                images.append(file)
-    return images
 
 @app.route('/')
 @app.route('/index')
 def index():
-    images = get_images()
-    return render_template("index.html", images=images, images_count=len(images))
+    return "Привет яндекс"
 
-@app.route('/load_photo', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('index'))
-    return render_template("index.html", images=get_images(), images_count=len(get_images()))
+
+@app.route('/member')
+def member():
+    with open("C:/Users/utu/PycharmProjects/pythonProject4/templates/crew_data.json" , "r", encoding="utf-8") as f:
+        crew = json.load(f)["crew"]
+        random_member = random.choice(crew)
+        random_member["speciality"] = ", ".join(sorted(random_member["specialties"]))
+        return render_template("index.html", member=random_member)
+
 
 if __name__ == '__main__':
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
     app.run(port=8080, host='127.0.0.1')
