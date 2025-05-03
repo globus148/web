120e78f44188774b251c3eafe29a4c8f0fecceb4
utu <gulovskiu@gmail.com>
2025-05-03 12:45:18 +0300

    Двойная защита

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField('id астронавта', validators=[DataRequired()])
+    password = PasswordField('Пароль автронавта', validators=[DataRequired()])
+    username1 = StringField('id капитана', validators=[DataRequired()])
+    password1 = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
 @app.route('/')
@@ -9,21 +22,12 @@ def index():
     return "Привет, Яндекс!"
 
 
-@app.route("/answer")
-@app.route('/auto_answer')
-def answer():
-    book = {
-        'title': 'Анкета претендента',
-        'surname': 'Иванов',
-        'name': 'Иван',
-        'education': 'Высшее',
-        'profession': 'Инженер-исследователь',
-        'sex': 'male',
-        'motivation': 'Хочу стать частью великой миссии!',
-        'ready': 'Да'
-    }
-
-    return render_template("auto_answer.html", **book)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Аварийный доступ', form=form)
 
 
 if __name__ == '__main__':
--- test.py
+++
@@ -1,5 +0,0 @@
-
-def reverse(s):
-    if type(s) != str:
-        raise TypeError('Expected str, got {}'.format(type(s)))
-    return s[::-1]
