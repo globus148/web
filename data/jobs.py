2f8a7e9939540131f75fe2c1b3f31de1bb1e9af9
utu <gulovskiu@gmail.com>
2025-05-09 20:26:15 +0300

    Журнал работ

---
+++ data/jobs.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from data.db_session import SqlAlchemyBase
+
+
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    team_leader = Column(Integer, ForeignKey("users.id"))
+    job = Column(String)
+    work_size = Column(Integer)
+    colloborators = Column(String)
+    start_date = Column(DateTime)
+    end_date = Column(DateTime)
+    if_finished = Column(Boolean)
+    team_leader_user = relationship("User")
--- main.py
+++ main.py
@@ -1,24 +1,19 @@
-from flask import Flask, url_for, render_template, request, redirect
-import json
-import random
+from flask import Flask, render_template
+from data.db_session import global_init, create_session
+from data.jobs import Jobs
+from data import db_session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
 def index():
-    return "Привет яндекс"
-
-
-@app.route('/member')
-def member():
-    with open("C:/Users/utu/PycharmProjects/pythonProject4/templates/crew_data.json" , "r", encoding="utf-8") as f:
-        crew = json.load(f)["crew"]
-        random_member = random.choice(crew)
-        random_member["speciality"] = ", ".join(sorted(random_member["specialties"]))
-        return render_template("index.html", member=random_member)
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    return render_template("index.html", jobs=jobs)
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    global_init("db/blogs.db")
+    app.run(port=8080, host="127.0.0.1")
